86b2ef3be19c410b00d404c39c12ee99586f3602
Eonics <Eonics@rutgers-mbp.kpn>
2023-06-23 15:47:30 +0200

    First commit

---
+++ Application.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Application {
+    public void run(){
+        String enteredName = new Scanner(System.in).nextLine();
+
+        System.out.println("Hello " + enteredName);
+    }
+}
